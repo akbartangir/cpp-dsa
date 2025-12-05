8ae1479c8f97121b2395984c4f20746f0aa0e230
Akbar <202290398@ajou.uz>
2025-12-05 09:06:29 +0500

    Add TreeNode class definition and BST and test_bst

---
+++ include/TreeNode.h
@@ -0,0 +1,15 @@
+#ifndef TREENODE_H
+#define TREENODE_H
+
+#include <utility>
+
+/// @brief A node in a tree structure.
+/// @tparam T The type of data stored in the node.
+///
+/// This class represents a single node in a tree,
+/// holding data and pointers to its children.
+template <typename T>
+class TreeNode
+{
+};
+#endif // TREENODE_H
